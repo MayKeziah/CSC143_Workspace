219ffd0a8020981437e3edabc5e7bdd7353d7036
MayKeziah <45299665+MayKeziah@users.noreply.github.com>
2019-02-01 17:22:04 -0800

    L3_ArrayLab: created main and print methods, body not populated

---
+++ Workspace/L3_ArrayLab/src/algorithms/AlgorithmClient.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package algorithms;
+
+/**
+ * @author E
+ *
+ */
+public class AlgorithmClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * @param list the list to print to the console
+	 */
+	public static void print(int[] list) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
