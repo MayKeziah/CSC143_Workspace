a89bcdcb4696f1ce7f0231dc99ff9ee63028f364
MayKeziah <keziah.may@gmail.com>
2019-02-23 16:07:15 -0800

    comment header intArrayList 01

--- Workspace/Proj_IntArrayList/src/version_01/IntArrayList.java
+++ Workspace/Proj_IntArrayList/src/version_01/IntArrayList.java
@@ -4,8 +4,10 @@
 package version_01;
 
 /**
+ * <p> An IntArrayList is a data structure for storing and manipulating a set of integers.
+ * 
+ * @version 01
  * @author E
- *
  */
 public class IntArrayList {
 
