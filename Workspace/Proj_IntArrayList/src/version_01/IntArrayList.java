00712b25f9397f7113a9c9fcfbc4fe469f6604af
MayKeziah <keziah.may@gmail.com>
2019-02-23 16:16:34 -0800

    IntArrayList 01 datafields created and commented

--- Workspace/Proj_IntArrayList/src/version_01/IntArrayList.java
+++ Workspace/Proj_IntArrayList/src/version_01/IntArrayList.java
@@ -10,5 +10,19 @@ package version_01;
  * @author E
  */
 public class IntArrayList {
+	
+
+	private int[] element; // The internal data structure.
+	private int size; 	   // The current number of elements stored in the IntArrayList
+	
+	/**
+	 * 	The initialized capacity for a default IntArrayList (10).
+	 * */
+	public static int DEFAULT_CAPACITY = 10; 
+			
+	/**
+	 * The value returned if an element is not found in the IntArrayList (-1).
+	 * */
+	public static int NOT_FOUND = -1;
 
 }
