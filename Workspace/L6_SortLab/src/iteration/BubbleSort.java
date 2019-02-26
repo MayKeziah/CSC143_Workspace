f6785340fd3e7c9204bb90d685d7ba48664bc122
MayKeziah <keziah.may@gmail.com>
2019-02-25 16:30:12 -0800

    filled in code for bubble sort

--- Workspace/L6_SortLab/src/iteration/BubbleSort.java
+++ Workspace/L6_SortLab/src/iteration/BubbleSort.java
@@ -10,7 +10,15 @@ public class BubbleSort {
 	 *  @param data array of integers.
 	 *********************************************************/
 	public static void bubbleSort(int[ ] data) {
-		
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data.length - i - 1; j ++) {
+				if (data[j] > data[j + 1]) {
+					int temp = data[j];
+					data[j] = data[j + 1];
+					data[j + 1] = temp;
+				}
+			}
+		}
 	}
 
 	/***************************************************************
