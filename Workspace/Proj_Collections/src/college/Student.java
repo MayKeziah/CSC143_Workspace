58d32260596af3b307585f4f4a7eefe8d114f10c
MayKeziah <keziah.may@gmail.com>
2019-03-13 01:11:51 -0700

    Generating student methods

--- Workspace/Proj_Collections/src/college/Student.java
+++ Workspace/Proj_Collections/src/college/Student.java
@@ -14,19 +14,6 @@ public class Student {
 	private Major major;
 	private double gpa;
 	
-	
-	public Student() {
-		this(null, 0, null, null, 0.0);
-	}
-	
-	public Student(String name, int age, Rank rank, Major major, double gpa) {
-		this.name 	= name;
-		this.age 	= age;
-		this.rank 	= rank;
-		this.major 	= major;
-		this.gpa 	= gpa;
-	}
-	
 	public Student(Builder builder) {
 		this.name 	= builder.name;
 		this.age 	= builder.age;
@@ -36,12 +23,63 @@ public class Student {
 	}
 
 	/**
-	 * @return the name
+	 * @param s1 a student to compare.
+	 * @param s2 the student to compare to s1.
+	 * @return 1 if the s1 is older than s2, 0 is they are the same age, and otherwise -1.
 	 */
-	public String getName() {
-		return name;
+	public static int compareByAge(Student s1, Student s2) {
+		if (s1.getAge() > s2.getAge()) { // s1 is older
+			return 1;
+		} if (s1.getAge() == s2.getAge()) { // Same age
+			return 0;
+		} return -1; // s1 is younger
 	}
-
+	
+	/**
+	 * @param s1 a student to compare.
+	 * @param s2 the student to compare to s1.
+	 * @return 1 if the first student's first name comes alphabetically before than the second, 0 is they are the same, and otherwise, -1.
+	 */
+	public static int compareByFirstName(Student s1, Student s2) {
+		if (s1.getFirstName() > s2.getFirstName()) { // s1 is older
+			return 1;
+		} if (s1.getFirstName() == s2.getFirstName()) { // Same age
+			return 0;
+		} return -1; // s1 is younger
+	}	
+	
+	/**
+	 * @param s1 a student to compare.
+	 * @param s2 the student to compare to s1.
+	 * @return 1 if the first student's GPA is greater than the second, 0 is they are equal, and otherwise, -1.
+	 */
+	public static int compareByGPA(Student s1, Student s2) {
+		return 0; //TODO
+	}
+	
+	/**
+	 * @param stud the student to compare to this student.
+	 * @return 1 if the student's last name is greater than the passed student, 0 is they are the same, and otherwise, -1.
+	 */
+	public int compareTo(Student stud) {
+		return 0; //TODO
+	}
+	
+	/**
+	 * @param other the object to compare to the this object.
+	 * @return returns true if the object passed is a Student object and has the same stored information as the current Student.
+	 */
+	public boolean equals(Object other) {
+		if (   (compareByAge	  (this, (Student) other) != 0)
+			&& (compareByFirstName(this, (Student) other) != 0)
+			&& (compareByGPA	  (this, (Student) other) != 0)
+			&& (compareTo		  		((Student) other) != 0)) 
+		{
+			return false;
+		}
+		return true;
+	}
+	
 	/**
 	 * @return the age
 	 */
@@ -50,10 +88,24 @@ public class Student {
 	}
 
 	/**
-	 * @return the rank
+	 * @return the student's first name
 	 */
-	public Rank getRank() {
-		return rank;
+	public String getFirstName() {
+		return ""; //TODO
+	}
+
+	/**
+	 * @return the GPA
+	 */
+	public double getGPA() {
+		return gpa;
+	}
+
+	/**
+	 * @return the student's first name
+	 */
+	public String getLastName() {
+		return ""; //TODO
 	}
 
 	/**
@@ -64,17 +116,17 @@ public class Student {
 	}
 
 	/**
-	 * @return the gpa
+	 * @return the name
 	 */
-	public double getGpa() {
-		return gpa;
+	public String getName() {
+		return name;
 	}
 
 	/**
-	 * @param name the name to set
+	 * @return the rank
 	 */
-	public void setName(String name) {
-		this.name = name;
+	public Rank getRank() {
+		return rank;
 	}
 
 	/**
@@ -85,10 +137,10 @@ public class Student {
 	}
 
 	/**
-	 * @param rank the rank to set
+	 * @param gpa the gpa to set
 	 */
-	public void setRank(Rank rank) {
-		this.rank = rank;
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
 	}
 
 	/**
@@ -99,12 +151,19 @@ public class Student {
 	}
 
 	/**
-	 * @param gpa the gpa to set
+	 * @param name the name to set
 	 */
-	public void setGpa(double gpa) {
-		this.gpa = gpa;
+	public void setName(String name) {
+		this.name = name;
 	}
-	
+
+	/**
+	 * @param rank the rank to set
+	 */
+	public void setRank(Rank rank) {
+		this.rank = rank;
+	}
+
 	public static class Builder{
 		private String 	name;
 		private int 	age;
